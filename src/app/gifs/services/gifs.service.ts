c0c0f9ed49924c21e266c9cce9a6237f9195de9e
Daniel Carbajal <edudani81@gmail.com>
2023-10-10 20:31:49 -0600

    Interfaces added to catch api response

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  public gifList: Gif[] = [];
   private _tagsHistory: string[] = [];
-  constructor() { }
+  private apikey: string = 'g7J2ZM2MMi0EEbXLWsnYqgRXSSOl34aF'; 
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag( tag: string ): void {
+  async searchTag( tag: string ): Promise<void> {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+    
+    const params = new HttpParams()
+    .set('api_key', this.apikey)
+    .set('limit', 10)
+    .set('q', tag);
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params: params })
+    .subscribe( resp => {
+      this.gifList = resp.data;
+      console.log({ gifs: this.gifList });
+    })
   }
 
   private organizeHistory(tag: string) {
